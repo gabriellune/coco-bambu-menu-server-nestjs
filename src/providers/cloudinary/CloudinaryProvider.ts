5812a83276c056e2f91bf78802afdb1dc495ced6
gabriellune <gabriellune@outlook.com>
2023-08-14 18:16:40 -0300

    fix(cloudinary): remove keys from provider

--- src/providers/cloudinary/CloudinaryProvider.ts
+++ src/providers/cloudinary/CloudinaryProvider.ts
@@ -1,13 +1,14 @@
 import { v2 } from 'cloudinary';
 import { CLOUDINARY } from './CloudinaryConstants';
+import 'dotenv/config';
 
 export const CloudinaryProvider = {
   provide: CLOUDINARY,
   useFactory: (): void => {
     v2.config({
-      cloud_name: 'dtbx4vjmv',
-      api_key: '757164822746622',
-      api_secret: 'GPpy1MH0Fbw2FRHJMkvG092YQR4',
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.API_KEY,
+      api_secret: process.env.API_SECRET,
     });
   },
 };
